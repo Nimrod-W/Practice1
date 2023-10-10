dcbfa64fe4700420a1b116ed53350dc10b1ab584
unknown <wanjohi.ngahu@gmail.com>
2023-10-10 03:20:29 -0700

    new practice repo

---
+++ app.js
@@ -0,0 +1,48 @@
+let playerText = document.querySelector('#playerText');
+let computerText = document.querySelector('#computerText');
+let resultText = document.querySelector('#resultText');
+const choiceBtns = document.querySelectorAll(".choiceBtn")
+let player;
+let computer;
+let result;
+
+
+
+choiceBtns.forEach(button => button.addEventListener('click', () => {
+    
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+
+} ));
+
+function computerTurn() {
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
+    }
+
+};
+
+function checkWinner() {
+    if(player == computer){
+        return "its a draw";
+    } else if( computer == "ROCK"){
+        return (player == "PAPER") ? "You Win" : "You lose"
+    } else if( computer == "PAPER"){
+        return (player == "SCISSORS") ? "You Win" : "You lose"
+    }else if( computer == "SCISSORS"){
+        return (player == "ROCK") ? "You Win" : "You lose"
+    }
+}
